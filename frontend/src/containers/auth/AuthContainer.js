9e7a9ab9be6cb500f43497cd796d5b799ad1a707
Dotsung <andy395023@gmail.com>
2019-05-22 17:49:16 +0900

    Auth page, container

---
+++ frontend/src/containers/auth/AuthContainer.js
@@ -0,0 +1,15 @@
+// @flow
+import React from 'react';
+import * as Style from './AuthContainerStyle';
+
+class AuthContainer extends React.Component {
+    render(){
+        return (
+            <Style.Wrapper>
+                aa
+            </Style.Wrapper>
+        )
+    }
+}
+
+export default AuthContainer;
---
+++ frontend/src/containers/auth/AuthContainerStyle.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+import oc from 'open-color';
+
+export const Wrapper = styled.div`
+
+`
