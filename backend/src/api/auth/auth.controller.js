5357b9900cb6d08eba1d7e61eaadc716cdbe0d01
Hwarang Choi <az8606@naver.com>
2019-05-27 19:56:16 +0900

    update register api(auth api 변경)

--- backend/src/api/auth/auth.controller.js
+++ backend/src/api/auth/auth.controller.js
@@ -6,41 +6,56 @@ import { generateToken }from '../../lib/token.js'
 import dotenv from 'dotenv';
 dotenv.config();
 
-// post로 받은 authCode의 값에 따라서 권한을 설정해주기 위한 함수( 0은 학생, 1은 선생님, 2는 게스트 )
-const authorizeUser = (authCode) => {
-    
-    if(authCode == process.env.studentCode)
-        return 0;
-    else if(authCode == process.env.teacherCode)
-        return 1;
-    else if(authCode == process.env.guestCode)
-        return 2;
-    else return 3;
+export const CheckAuth = async (ctx) => {
+    let auth;
+    switch(ctx.request.body.authCode){
+        case process.env.studentCode:
+            auth = 0;
+            break;
+        case process.env.teacherCode:
+            auth = 1;
+            break;
+        case process.env.guestCode:
+            auth = 2;
+            break;
+        default:
+            console.log(`CheckAuth - 지정되지 않은 권한코드입니다. / 입력된 권한코드 : ${ctx.request.body.authCode}`);
+            ctx.status = 400;
+            ctx.body = {
+                "error" : "001"
+            }
+            return;
+    }
+
+    ctx.body = {
+        "auth" : auth
+    };
 }
 
+export const RegStudent = async (ctx) => {
 
-
-// 회원가입 함수 ( auth의 index를 보면 /register 라는 URI로 들어왔을 때 처리하는 함수라는 것을 알 수 있음.)
-export const Register = async (ctx) => {
-    
     // Joi 라이브러리를 활용해서 형식을 검사하기 위해 객체를 하나 만들어 줌.
     // 왼쪽의 각 name에 해당하는 값의 요구조건임을 알 수 있다.
     // id를 예시로 들자면 '형식은 string이고, 입력 가능한 것은 alphanum( a-z, A-Z, 0-9 )이고, 최소 6글자 최대 30글자, 무조건 입력되야한다'라는 것을 알 수 있다.
     const Registeration = Joi.object().keys({
         id : Joi.string().alphanum().min(5).max(50).required(),
         password : Joi.string().min(5).max(50).required(),
-        authCode : Joi.string().alphanum().required()  
+        grade : Joi.string().max(1).required(),
+        class : Joi.string().max(1).required(),
+        number : Joi.string().max(2).required(),
+        name : Joi.string().min(2).max(50).required(),
+        phone : Joi.string().max(11).required(),
+        email : Joi.string().required()
     });
 
-    // 위에서 만든 Joi 객체를 이용해서 사용자가 입력한 정보(ctx.request.body)와 비교한다.
-    const firstResult = Joi.validate(ctx.request.body, Registeration);
+    const result = Joi.validate(ctx.request.body, Registeration);
 
     // 비교한 뒤 만약 에러가 발생한다면 400 에러코드를 전송하고, body에 001 이라는 내용(우리끼리의 오류 코드 약속)을 담아 joi 오류임을 알려줌
-    if(firstResult.error) {
-        console.log("Register - Joi 형식 에러")
+    if(result.error) {
+        console.log("RegStudent - Joi 형식 에러")
         ctx.status = 400;
         ctx.body = {
-            "error" : "001"
+            "error" : "002"
         }
         return;
     }
@@ -56,22 +71,17 @@ export const Register = async (ctx) => {
     // exist의 길이가 0이 아니라면, 중복된 아이디가 있다는 뜻
     // 따라서 만약 중복된 아이디가 있다면, 400 에러코드를 전송하고, body에 002라는 내용을 담아서 보냄
     if(exist.length){
-        console.log(`Register - 이미 존재하는 아이디입니다. / 입력된 아이디 : ${ctx.request.body.id}`);
+        console.log(`RegStudent - 이미 존재하는 아이디입니다. / 입력된 아이디 : ${ctx.request.body.id}`);
 
         ctx.status = 400;
         ctx.body = {
-            "error" : "002"
+            "error" : "003"
         }
         return;
     }
 
     // 데이터베이스에 저장할 내용을 정리하는 코드
-    const id = ctx.request.body.id;
 
-    // 비밀번호를 crypto 모듈을 이용해서 암호화해줌.
-    const password = ctx.request.body.password;
-    const password2 = crypto.createHmac('sha256', process.env.Password_KEY).update(password).digest('hex');
-    
     // userCode는 U0001 과 같은 5글자로 표현되는데, 이를 위해서 account 테이블의 모든 데이터를 받아온 후, length로 갯수를 셈.
     // 그 후 for문으로 부족한 0 갯수를 채워주고, 맨 앞에 U를 붙임
     const users = await account.findAll();
@@ -82,13 +92,80 @@ export const Register = async (ctx) => {
     }
 
     userCode = "U" + userCode;
+    
+    const id = ctx.request.body.id;
 
-    // 사용자가 입력한 코드와 설정한 코드가 일치하는 지 확인하고, 값을 보내줌
-    const auth = authorizeUser(ctx.request.body.authCode);
+    // 비밀번호를 crypto 모듈을 이용해서 암호화해줌.
+    const password2 = crypto.createHmac('sha256', process.env.Password_KEY).update(ctx.request.body.password).digest('hex');
 
-    // 만약 auth가 3이라면 지정하지 않은 유저코드이므로, 회원가입을 못 하게끔 함
-    if(auth == 3){
-        console.log(`Register - 존재하지 않는 유저코드입니다. / 입력된 유저코드 : ${ctx.request.body.authCode}`);
+
+    // 학생 코드는 0이기 때문에 auth 를 0으로 지정해 줌.
+    const auth = 0;
+
+    console.log(`ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ\nid 값 : ${id}\npassword 값 : ${ctx.request.body.password}\nhash 값 : ${password2}\nuserCode 값 : ${userCode}\nauth 값 : ${auth}\nㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ`);
+    
+    // 데이터베이스에 값을 저장함.
+    account.create({
+        "userCode" : userCode,
+        "id" : id,
+        "password" : password2,
+        "auth" : auth
+    });
+
+    console.log(`RegStudent - 새로운 회원이 저장되었습니다. / 아이디 : ${id}`);
+
+    student.create({
+        "userCode" : userCode,
+        "grade" : ctx.request.body.grade,
+        "class" : ctx.request.body.class,
+        "number" : ctx.request.body.number,
+        "name" : ctx.request.body.name,
+        "phone" : ctx.request.body.phone,
+        "email" : ctx.request.body.email,
+        "greenpoint" : 0,
+        "redpoint" : 0
+    });
+
+    console.log(`RegStudent - 학생 정보를 등록하였습니다. / 유저코드 : ${userCode}`);
+}
+
+export const RegTeacher = async (ctx) => {
+
+    // Joi 라이브러리를 활용해서 형식을 검사하기 위해 객체를 하나 만들어 줌.
+    // 왼쪽의 각 name에 해당하는 값의 요구조건임을 알 수 있다.
+    // id를 예시로 들자면 '형식은 string이고, 입력 가능한 것은 alphanum( a-z, A-Z, 0-9 )이고, 최소 6글자 최대 30글자, 무조건 입력되야한다'라는 것을 알 수 있다.
+    const Registeration = Joi.object().keys({
+        id : Joi.string().alphanum().min(5).max(50).required(),
+        password : Joi.string().min(5).max(50).required(),
+        name : Joi.string().min(2).max(50).required(),
+        phone : Joi.string().max(11).required(),
+        email : Joi.string().required()
+    });
+
+    const result = Joi.validate(ctx.request.body, Registeration);
+
+    // 비교한 뒤 만약 에러가 발생한다면 400 에러코드를 전송하고, body에 001 이라는 내용(우리끼리의 오류 코드 약속)을 담아 joi 오류임을 알려줌
+    if(result.error) {
+        console.log("RegTeacher - Joi 형식 에러")
+        ctx.status = 400;
+        ctx.body = {
+            "error" : "002"
+        }
+        return;
+    }
+
+    // 아이디 중복체크
+    // 사용자가 입력한 id와 동일한 id가 데이터베이스에 있는지 검사하고, 만약 있다면 exist에 저장함
+    const exist = await account.findAll({
+        where: {
+            id : ctx.request.body.id
+        }
+    });
+
+    // exist의 길이가 0이 아니라면, 중복된 아이디가 있다는 뜻
+    // 따라서 만약 중복된 아이디가 있다면, 400 에러코드를 전송하고, body에 002라는 내용을 담아서 보냄
+    if(exist.length){
+        console.log(`RegTeacher - 이미 존재하는 아이디입니다. / 입력된 아이디 : ${ctx.request.body.id}`);
 
         ctx.status = 400;
         ctx.body = {
@@ -97,35 +174,135 @@ export const Register = async (ctx) => {
         return;
     }
 
-    // 입력된 데이터에 대한 로그를 한번 찍어봄
-    console.log(`ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ\nid 값 : ${id}\npassword 값 : ${password}\nhash 값 : ${password2}\nuserCode 값 : ${userCode}\nauth 값 : ${auth}\nㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ`);
+    // 데이터베이스에 저장할 내용을 정리하는 코드
+
+    // userCode는 U0001 과 같은 5글자로 표현되는데, 이를 위해서 account 테이블의 모든 데이터를 받아온 후, length로 갯수를 셈.
+    // 그 후 for문으로 부족한 0 갯수를 채워주고, 맨 앞에 U를 붙임
+    const users = await account.findAll();
+    let userCode = (users.length+1).toString();
+
+    for(var i=0;userCode.length<4;i++){
+        userCode = "0" + userCode;
+    }
+
+    userCode = "U" + userCode;
+    
+    const id = ctx.request.body.id;
+
+    // 비밀번호를 crypto 모듈을 이용해서 암호화해줌.
+    const password2 = crypto.createHmac('sha256', process.env.Password_KEY).update(ctx.request.body.password).digest('hex');
+
+
+    // 선생 코드는 1이기 때문에 auth 를 1으로 지정해 줌.
+    const auth = 1;
+
+    console.log(`ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ\nid 값 : ${id}\npassword 값 : ${ctx.request.body.password}\nhash 값 : ${password2}\nuserCode 값 : ${userCode}\nauth 값 : ${auth}\nㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ`);
     
     // 데이터베이스에 값을 저장함.
     account.create({
-        id : id,
-        password : password2,
-        userCode : userCode,
-        auth : auth
+        "userCode" : userCode,
+        "id" : id,
+        "password" : password2,
+        "auth" : auth
     });
 
-    console.log(`새로운 회원이 저장되었습니다. / 아이디 : ${id}`);
+    console.log(`RegTeacher - 새로운 회원이 저장되었습니다. / 아이디 : ${id}`);
 
-    ctx.body = "성공하였습니다.";
+    teacher.create({
+        "userCode" : userCode,
+        "name" : ctx.request.body.name,
+        "phone" : ctx.request.body.phone,
+        "email" : ctx.request.body.email
+    });
 
-    const payload = {
-        id : id,
-        userCode : userCode,
-        auth : auth
-    };
+    console.log(`RegTeacher - 선생 정보를 등록하였습니다. / 유저코드 : ${userCode}`);
+}
 
-    let token = null;
-    token = await generateToken(payload);
+export const RegGuest = async (ctx) => {
 
-    console.log(token);
+    // Joi 라이브러리를 활용해서 형식을 검사하기 위해 객체를 하나 만들어 줌.
+    // 왼쪽의 각 name에 해당하는 값의 요구조건임을 알 수 있다.
+    // id를 예시로 들자면 '형식은 string이고, 입력 가능한 것은 alphanum( a-z, A-Z, 0-9 )이고, 최소 6글자 최대 30글자, 무조건 입력되야한다'라는 것을 알 수 있다.
+    const Registeration = Joi.object().keys({
+        id : Joi.string().alphanum().min(5).max(50).required(),
+        password : Joi.string().min(5).max(50).required(),
+        name : Joi.string().min(2).max(50).required(),
+        email : Joi.string().required()
+    });
 
-    ctx.body = {
-        token : token
-    };
+    const result = Joi.validate(ctx.request.body, Registeration);
+
+    // 비교한 뒤 만약 에러가 발생한다면 400 에러코드를 전송하고, body에 001 이라는 내용(우리끼리의 오류 코드 약속)을 담아 joi 오류임을 알려줌
+    if(result.error) {
+        console.log("RegGuest - Joi 형식 에러")
+        ctx.status = 400;
+        ctx.body = {
+            "error" : "002"
+        }
+        return;
+    }
+
+    // 아이디 중복체크
+    // 사용자가 입력한 id와 동일한 id가 데이터베이스에 있는지 검사하고, 만약 있다면 exist에 저장함
+    const exist = await account.findAll({
+        where: {
+            id : ctx.request.body.id
+        }
+    });
+
+    // exist의 길이가 0이 아니라면, 중복된 아이디가 있다는 뜻
+    // 따라서 만약 중복된 아이디가 있다면, 400 에러코드를 전송하고, body에 002라는 내용을 담아서 보냄
+    if(exist.length){
+        console.log(`RegGuest - 이미 존재하는 아이디입니다. / 입력된 아이디 : ${ctx.request.body.id}`);
+
+        ctx.status = 400;
+        ctx.body = {
+            "error" : "003"
+        }
+        return;
+    }
+
+    // 데이터베이스에 저장할 내용을 정리하는 코드
+
+    // userCode는 U0001 과 같은 5글자로 표현되는데, 이를 위해서 account 테이블의 모든 데이터를 받아온 후, length로 갯수를 셈.
+    // 그 후 for문으로 부족한 0 갯수를 채워주고, 맨 앞에 U를 붙임
+    const users = await account.findAll();
+    let userCode = (users.length+1).toString();
+
+    for(var i=0;userCode.length<4;i++){
+        userCode = "0" + userCode;
+    }
+
+    userCode = "U" + userCode;
+    
+    const id = ctx.request.body.id;
+
+    // 비밀번호를 crypto 모듈을 이용해서 암호화해줌.
+    const password2 = crypto.createHmac('sha256', process.env.Password_KEY).update(ctx.request.body.password).digest('hex');
+
+
+    // 게스트 코드는 2이기 때문에 auth 를 2로 지정해 줌.
+    const auth = 2;
+
+    console.log(`ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ\nid 값 : ${id}\npassword 값 : ${ctx.request.body.password}\nhash 값 : ${password2}\nuserCode 값 : ${userCode}\nauth 값 : ${auth}\nㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ`);
+    
+    // 데이터베이스에 값을 저장함.
+    account.create({
+        "userCode" : userCode,
+        "id" : id,
+        "password" : password2,
+        "auth" : auth
+    });
+
+    console.log(`RegGuest - 새로운 회원이 저장되었습니다. / 아이디 : ${id}`);
+
+    guest.create({
+        "userCode" : userCode,
+        "name" : ctx.request.body.name,
+        "email" : ctx.request.body.email
+    });
+
+    console.log(`RegGuest - 선생 정보를 등록하였습니다. / 유저코드 : ${userCode}`);
 }
 
 export const Login = async (ctx) => {
