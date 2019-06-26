366b765ac1e148f1f2191232030d0ce7c682ed47
Hwarang Choi <az8606@naver.com>
2019-06-26 13:47:47 +0900

    add meal api

--- backend/src/api/school/index.js
+++ backend/src/api/school/index.js
@@ -1,8 +1,9 @@
 import Router from 'koa-router';
-import { DefaultMeal } from './food.controller';
+import { DefaultMeal, SelectMeal } from './food.controller';
 
 const school = new Router();
 
 school.get('/meal/this', DefaultMeal);
+school.post('/meal/select', SelectMeal);
 
 export default school;
